d0e4104d8ef901650474914013da0eee163dd0dd
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2022-12-24 14:42:55 +0200

    day2

--- Fundamentals Of Programming With C++/app.cpp
+++ Fundamentals Of Programming With C++/app.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+// int main()
+// {
+//   std::cout << "now we are programming";
+//   return 0;
+// }
+
+
 int main()
 {
-  std::cout << "now we are programming";
+  std::cout << "now we are programming 1\n";
+  std::cout << "now we are programming 2\n";
+  std::cout << "now we are programming 3\n";
   return 0;
 }
